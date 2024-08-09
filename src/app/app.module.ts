a3e2d209078d3d3a60d342131909a27fabc34c9b
paracus2 <rodrigo-dru@hotmail.com>
2024-08-09 18:13:48 -0300

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
